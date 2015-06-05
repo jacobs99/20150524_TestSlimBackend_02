00084dc7bb384609764d95c7a595f37aa07215a8
jacobs99 <jacobs99@users.noreply.github.com>
2015-06-05 08:53:22 +0200

    removed old CORS Code

--- api/index.php
+++ api/index.php
@@ -12,35 +12,6 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim();
 $app = \Slim\Slim::getInstance();
 
-/*
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (
-            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' ||
-            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'GET' ||
-            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' ||
-            $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT' )) {
-
-
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Credentials: true");
-        header('Access-Control-Allow-Headers: X-Requested-With');
-        header('Access-Control-Allow-Headers: Content-Type');
-        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
-        header('Access-Control-Max-Age: 86400');
-
-    }
-    exit;
-}
-
-*/
 
 
 $app->options('/square/:val', function () use($app) {
